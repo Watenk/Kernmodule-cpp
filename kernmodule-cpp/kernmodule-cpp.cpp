bf154c01770c76fe95d8efbab577134015681c79
Watenk <Ivar.nuij@gmail.com>
2023-05-10 13:28:35 +0200

    Added project and SFML

---
+++ kernmodule-cpp/kernmodule-cpp.cpp
@@ -0,0 +1,14 @@
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "test program");
+    window.setFramerateLimit(144);
+
+    return 0;
+}
